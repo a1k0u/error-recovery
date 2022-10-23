9c313fb5de78efd984c5d71fc0ddba77001b12fc
a1k0u <alko7enko@gmail.com>
2022-10-23 05:49:55 +0300

    configurate run test script

--- phrase-mode/run_tests.sh
+++ phrase-mode/run_tests.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 FLAG=0
-for TESTFILE in "$@"; do
-    OUTFILE="${TESTFILE}.out"
-    EXPECTEDFILE="${TESTFILE}.expected"
-    python3 parse.py $TESTFILE
+for i in `seq 7`; do
+    TESTFILE="./tests/test${i}.txt"
+    OUTFILE="./tests/test${i}.txt.out"
+    EXPECTEDFILE="./tests/test${i}.txt.expected"
+
+    python3 parse.py $TESTFILE > $OUTFILE
+
     if ! cmp -s "$OUTFILE" "$EXPECTEDFILE"; then
         FLAG=1
-	echo "Test failed: ${TESTFILE}"	
+	      echo "Test failed: ${TESTFILE}"
     fi
-    rm $OUTFILE    
+
+    rm $OUTFILE
 done
+
 if [[ $FLAG != 1 ]]; then
     echo "Test passed"
 fi
